135cd14cd856280bac7081e6e6334956392413bc
nkharlamova <nata.kocto@gmail.com>
2022-03-20 00:09:17 +0200

    have added model Source, created form for it

---
+++ app/currency/forms.py
@@ -0,0 +1,9 @@
+from currency.models import Source
+
+from django import forms
+
+
+class SourceForm(forms.ModelForm):
+    class Meta:
+        model = Source
+        fields = ('source_url', 'name')
--- app/currency/views.py
+++ app/currency/views.py
@@ -1,6 +1,8 @@
-from currency.models import ContactUs, Rate
+from currency.forms import SourceForm
+from currency.models import ContactUs, Rate, Source
 
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
 
 def contacts_list(request):
@@ -15,3 +17,40 @@ def rate_list(request):
 
 def index(request):
     return render(request, 'index.html')
+
+
+def source_list(request):
+    source = Source.objects.all().order_by('-id')
+    return render(request, 'source.html', context={'source_list': source})
+
+
+def source_create(request):
+    if request.method == 'POST':
+        form = SourceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/source-list/')
+    else:
+        form = SourceForm()
+    return render(request, 'source_create.html', context={'form': form})
+
+
+def source_update(request, pk):
+    instance = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        form = SourceForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/source-list/')
+    else:
+        form = SourceForm(instance=instance)
+    return render(request, 'source_update.html', context={'form': form})
+
+
+def source_delete(request, pk):
+    instance = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        instance.delete()
+        return HttpResponseRedirect('/source-list/')
+    else:
+        return render(request, 'source_delete.html', context={'source': instance})
